e8de26e554d3b05b16d19d76c0434c4568f28a65
Invixvb <le.van.boxtel@sintlucasedu.nl>
2020-12-09 13:31:54 +0100

    CameraSwitch laten werken
    Moet nog fixen dat die op 1 knop werkt

---
+++ FoxGameTowerDefence/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+	public GameObject camera1;
+	public GameObject camera2;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			camera1.SetActive(false);
+			camera2.SetActive(true);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			camera1.SetActive(true);
+			camera2.SetActive(false);
+		}
+	}
+}
