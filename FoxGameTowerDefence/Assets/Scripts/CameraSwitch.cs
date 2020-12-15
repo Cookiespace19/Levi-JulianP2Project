9e0f9df51daf12839fa98a0c063cc4dd286c9b65
Invixvb <74636900+Invixvb@users.noreply.github.com>
2020-12-15 10:27:38 +0100

    CameraSwitch done
    Ik ga nu door met movement

--- FoxGameTowerDefence/Assets/Scripts/CameraSwitch.cs
+++ FoxGameTowerDefence/Assets/Scripts/CameraSwitch.cs
@@ -6,19 +6,21 @@ public class CameraSwitch : MonoBehaviour
 {
 	public GameObject camera1;
 	public GameObject camera2;
+	bool standardCamera = true;
+
+	private void  Start()
+	{
+		camera1.active = standardCamera;
+		camera2.active = !standardCamera;
+	}
 
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Alpha1))
 		{
-			camera1.SetActive(false);
-			camera2.SetActive(true);
-		}
-
-		if (Input.GetKeyDown(KeyCode.Alpha2))
-		{
-			camera1.SetActive(true);
-			camera2.SetActive(false);
+			standardCamera = !standardCamera;
+			camera1.active = standardCamera;
+			camera2.active = !standardCamera;
 		}
 	}
 }
