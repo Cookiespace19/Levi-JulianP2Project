8d915b08fabedff96c7f98883fa0f1989f8e3b1c
Invixvb <74636900+Invixvb@users.noreply.github.com>
2020-12-15 14:06:52 +0100

    TreeHealth + EnemyHealth

---
+++ FoxGameTowerDefence/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float enemyHealth;
+    public float enemyMaxHealth;
+
+    public Slider slider;
+
+    private void Start()
+    {
+        enemyHealth = enemyMaxHealth;
+        slider.value = CalculateEnemyHealth();
+    }
+
+    private void Update()
+    {
+        slider.value = CalculateEnemyHealth();
+
+        if(enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if(enemyHealth > enemyMaxHealth)
+        {
+            enemyHealth = enemyMaxHealth;
+        }
+    }
+
+    float CalculateEnemyHealth()
+    {
+        return enemyHealth / enemyMaxHealth;
+    }
+}
---
+++ FoxGameTowerDefence/Assets/Scripts/TreeHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeHealth : MonoBehaviour
+{
+    public float treeHealth;
+    public float treeMaxHealth;
+
+    public Slider slider;
+
+    private void Start()
+    {
+        treeHealth = treeMaxHealth;
+        slider.value = CalculateTreeHealth();
+    }
+
+    private void Update()
+    {
+        slider.value = CalculateTreeHealth();
+
+        if (treeHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (treeHealth > treeMaxHealth)
+        {
+            treeHealth = treeMaxHealth;
+        }
+    }
+
+    float CalculateTreeHealth()
+    {
+        return treeHealth / treeMaxHealth;
+    }
+}
