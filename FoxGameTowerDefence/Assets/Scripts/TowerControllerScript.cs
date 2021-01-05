41784f35faa780fbdd7446a58759be0f7dfe4eb6
Julian Snelders <j.snelders@outlook.com>
2021-01-05 16:03:09 +0100

    further terrain and shrine

--- FoxGameTowerDefence/Assets/Scripts/TowerControllerScript.cs
+++ FoxGameTowerDefence/Assets/Scripts/TowerControllerScript.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class TowerControllerScript : MonoBehaviour
 {
-    public GameObject MenuActive;
-    public GameObject MenuInActive;
-    [SerializeField] bool menuActiveBool = false;
+    public GameObject Player;
 
     public GameObject camera1;
     public GameObject camera2;
     [SerializeField] bool standardCamera = true;
 
+    public Transform TPOne;
+    public Transform TPTwo;
+    public Transform TPThree;
+    public Transform TPFour;
+    public Transform TPFive;
+
+    public GameObject TP_UI;
+
     private void Start()
     {
-        MenuActive.active = menuActiveBool;
-        MenuInActive.active = !menuActiveBool;
 
         camera1.active = standardCamera;
         camera2.active = !standardCamera;
@@ -25,15 +29,8 @@ public class TowerControllerScript : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            if (Input.GetButtonDown("TabMenu"))
-			{
-                print("Tab");
-                menuActiveBool = !menuActiveBool;
-                MenuActive.active = menuActiveBool;
-                MenuInActive.active = !menuActiveBool;
-			}
 
-            if (Input.GetButtonDown("CamChange"))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 print("Camera");
                 standardCamera = !standardCamera;
@@ -43,5 +40,43 @@ public class TowerControllerScript : MonoBehaviour
         }
 
         //als je op een button klikt van de controller, dan instantiate die een dier en sluit die het menu af. (Om een beer te spawnen moet het menu active zijn.)
+    }
+
+    public void Update()
+    {
+
+        if (camera1.active)
+        {
+            TP_UI.SetActive(false);
+            Player.GetComponent<playerMovement>().enabled = true;
+        }
+
+        if (camera2.active)
+        {
+            TP_UI.SetActive(true);
+            Player.GetComponent<playerMovement>().enabled = false;
+        }
+    }
+
+
+    void TP1()
+    {
+        Player.transform.position = TPOne.transform.position;
+    }
+    void TP2()
+    {
+        Player.transform.position = TPTwo.transform.position;
+    }
+    void TP3()
+    {
+        Player.transform.position = TPThree.transform.position;
+    }
+    void TP4()
+    {
+        Player.transform.position = TPFour.transform.position;
+    }
+    void TP5()
+    {
+        Player.transform.position = TPFive.transform.position;
     }
 }
